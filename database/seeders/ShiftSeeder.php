ba551991a2d2cf1025b62032f2f49960b638561a
Victor Ukam <victorjohnukam@gmail.com>
2022-11-25 13:07:49 +0000

    Updated ShifSeeder.php

--- database/seeders/ShiftSeeder.php
+++ database/seeders/ShiftSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Shift;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class ShiftSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Shift::factory(1)->create();
     }
 }
