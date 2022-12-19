004f6981a3bc4b873de0bfa5fd747e076a167fa1
Victor Ukam <victorjohnukam@gmail.com>
2022-12-19 23:01:44 +0100

    Quick update to Certification and Nex of Kin Models

--- app/Models/Certification.php
+++ app/Models/Certification.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Certification extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function driver(): BelongsTo
+    {
+        return $this->belongsTo(Driver::class);
+    }
+
+    protected $casts = [
+        'issue_date' => 'date',
+        'expiry_date' => 'date',
+    ];
 }
--- app/Models/NextOfKin.php
+++ app/Models/NextOfKin.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class NextOfKin extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function driver(): BelongsTo
+    {
+        return $this->belongsTo(Driver::class);
+    }
 }
