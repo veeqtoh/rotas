1f7a69511c26949ad7c41642f88ea956adfe06e2
Victor John Ukam <victorjohnukam@gmail.com>
2022-12-22 15:02:26 +0000

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'api_version' => \App\Http\Middleware\APIversion::class,
+        'api_version' => \App\Http\Middleware\APIVersion::class,
         'admin' => \App\Http\Middleware\CheckAdmin::class,
         'checkPassword' => \App\Http\Middleware\CheckPassword::class,
     ];
